b2c356589aff5db7abe60d984384a2285d2086ad
Hồ Ngọc Hoàng Anh <149872171+Titapie@users.noreply.github.com>
2025-12-17 23:42:37 +0700

    Update useCartStore.js

--- adtcar2/src/store/useCartStore.js
+++ adtcar2/src/store/useCartStore.js
@@ -1,20 +1,20 @@
 import { create } from 'zustand';
 import axiosClient from './axiosClient'; 
 
-// Store này quản lý số lượng giỏ hàng toàn cục
+
 export const useCartStore = create((set) => ({
   cartCount: 0, // Biến lưu số lượng
 
-  // Hàm gọi API để cập nhật số lượng mới nhất
+
   fetchCart: async () => {
     try {
-      // Thêm ?t=... để chống Cache triệt để
+     
       const response = await axiosClient.get(`/user/get/cart?t=${Date.now()}`);
       
       const dataObj = response.data ? response.data : response;
       const listItems = dataObj.data || [];
 
-      // Cập nhật vào store
+  
       set({ cartCount: listItems.length });
     } catch (error) {
       console.error("Lỗi cập nhật giỏ hàng:", error);
@@ -22,6 +22,6 @@ export const useCartStore = create((set) => ({
     }
   },
 
-  // Hàm reset khi đăng xuất
+
   resetCart: () => set({ cartCount: 0 }),
 }));
